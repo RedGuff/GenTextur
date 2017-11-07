41565c06de74d3bccc500e26b18cdfd00ed24e87
Saussereau Dominique <domsau2@yahoo.fr>
2017-11-07 19:54:47 +0100

    Create GenTextur.cpp
    Create white texture.

---
+++ GenTextur.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+int main() {
+    cout << "Bonjour.\nCeci fabrique des textures tuilables rectangulaires. " << endl;
+    cout << "Une nouvelle image pour chaque fois, en remplacement." << endl;
+    cout << "Internationnalisation future probable. (Je sais : anglais langue mondiale... etc...)" << endl;
+    cout << "Le format est PGM (PPM gris ASCII : https://fr.wikipedia.org/wiki/Portable_pixmap)." << endl;
+    cout << "Vous pouvez utiliser un logiciel de type GIMP pour les couleurs et la conversion. (https://www.gimp.org/)" << endl;
+    // Par défaut :
+    const int MAXBlancMAX = 65536; // Norme du fichier.
+    const int FULLWHITE = 256; // < MAXBlancMAX( Norme du fichier ).
+
+    int X=16; // Colonnes
+    int Y=16; // Lignes // 70 caractères MAX par ligne.
+    bool GTend=true; // Ne pas recommencer.
+    do {
+        // Début du fichier :
+        int PixFin=0;
+        string const nomFichier("GenTex.ppm"); // input ?
+        ofstream myfile;
+        myfile.open (nomFichier.c_str());
+        myfile << "P2\n";
+        myfile << "# GenTextur\n";
+        myfile << X ;
+        string separator = " ";
+        myfile << separator;
+        myfile << Y;
+        myfile << "\n";
+        myfile << Y;
+        myfile << "\n";
+        // Si pas de ligne de commande.
+// input X (que nombre) 1 à 32k.
+// input Y (que nombre) 1 à 32k.
+// input nom.
+        for (int i=0; i<X; i++) {
+            for (int j=0; j<Y; j++) {
+                // formule avec rand.
+                PixFin=FULLWHITE;
+                myfile << PixFin;
+                if (j<Y -1) {
+                    myfile << " "; // Pas en fin de ligne.
+                    }
+                }
+            if (i<X -1) {
+                myfile << "\n";  // Pas en fin de fichier.
+                }
+            }
+        myfile.close();
+        // input end?
+        }
+    while (!GTend);
+    cout << "Au revoir." << endl;
+    return 0;
+    }

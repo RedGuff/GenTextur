c0d2739f95bed0c63a87d74558cd530e31178c64
Saussereau Dominique <domsau2@yahoo.fr>
2017-11-07 21:45:26 +0100

    Update GenTextur.cpp
    Debug.

--- GenTextur.cpp
+++ GenTextur.cpp
@@ -10,11 +10,11 @@ int main() {
     cout << "Bonjour.\nCeci fabrique des textures tuilables rectangulaires. " << endl;
     cout << "Une nouvelle image pour chaque fois, en remplacement." << endl;
     cout << "Internationnalisation future probable. (Je sais : anglais langue mondiale... etc...)" << endl;
-    cout << "Le format est PGM (PPM gris ASCII : https://fr.wikipedia.org/wiki/Portable_pixmap)." << endl;
+    cout << "Le format est PGM (Netpbm gris ASCII : https://fr.wikipedia.org/wiki/Portable_pixmap)." << endl;
     cout << "Vous pouvez utiliser un logiciel de type GIMP pour les couleurs et la conversion. (https://www.gimp.org/)" << endl;
     // Par défaut :
-    const int FULLWHITE = 256;
-    if (FULLWHITE>65536 ){// ( Norme du fichier ).
+    const int FULLWHITE = 256; // From 1 (included) to 65536 (included).
+    if ((FULLWHITE>65536 )||(FULLWHITE<1 )){// ( Norme du fichier ).
 cerr << "Erreur de fichier !";
 
 ;
@@ -25,9 +25,9 @@ cerr << "Erreur de fichier !";
     do {
         // Début du fichier :
         int PixFin=0;
-        string const nomFichier("GenTex.ppm"); // input ?
+        string const nomFichier("GenTex.pgm"); // input ?
         ofstream myfile;
-        myfile.open (nomFichier.c_str());
+        myfile.open (nomFichier.c_str()); // .c_str() because not "GenTex.pgm".
         myfile << "P2\n";
         myfile << "# GenTextur\n";
         myfile << X ;

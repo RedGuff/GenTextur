92eb9353d1c3125a75c0ef9b5d6d59086976de70
Saussereau Dominique <domsau2@yahoo.fr>
2017-11-07 20:21:42 +0100

    Not full white, but random gray.
    + Bug corrected.

--- GenTextur.cpp
+++ GenTextur.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <stdio.h>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 int main() {
+    srand(time(NULL)); // Start rand.
     cout << "Bonjour.\nCeci fabrique des textures tuilables rectangulaires. " << endl;
     cout << "Une nouvelle image pour chaque fois, en remplacement." << endl;
     cout << "Internationnalisation future probable. (Je sais : anglais langue mondiale... etc...)" << endl;
     cout << "Le format est PGM (PPM gris ASCII : https://fr.wikipedia.org/wiki/Portable_pixmap)." << endl;
     cout << "Vous pouvez utiliser un logiciel de type GIMP pour les couleurs et la conversion. (https://www.gimp.org/)" << endl;
     // Par défaut :
-    const int MAXBlancMAX = 65536; // Norme du fichier.
-    const int FULLWHITE = 256; // < MAXBlancMAX( Norme du fichier ).
+    const int FULLWHITE = 256;
+    if (FULLWHITE>65536 ){// ( Norme du fichier ).
+cerr << "Erreur de fichier !";
 
+;
+    }
     int X=16; // Colonnes
-    int Y=16; // Lignes // 70 caractères MAX par ligne.
+    int Y=7; // Lignes // 70 caractères MAX par ligne.
     bool GTend=true; // Ne pas recommencer.
     do {
         // Début du fichier :
@@ -28,7 +35,7 @@ int main() {
         myfile << separator;
         myfile << Y;
         myfile << "\n";
-        myfile << Y;
+        myfile << FULLWHITE;
         myfile << "\n";
         // Si pas de ligne de commande.
 // input X (que nombre) 1 à 32k.
@@ -37,7 +44,8 @@ int main() {
         for (int i=0; i<X; i++) {
             for (int j=0; j<Y; j++) {
                 // formule avec rand.
-                PixFin=FULLWHITE;
+                PixFin= rand()%FULLWHITE;
+
                 myfile << PixFin;
                 if (j<Y -1) {
                     myfile << " "; // Pas en fin de ligne.
